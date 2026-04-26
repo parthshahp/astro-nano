3b09738850787c95920d51d211eca5b1abd6e79a
Parth Shah <parthshahp1@gmail.com>
2026-04-26 15:25:06 -0400

    add dark and light code blocks

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,18 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   site: "https://astro-nano-demo.vercel.app",
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx({ optimize: true }), sitemap()],
+  vite: { plugins: [tailwindcss()] },
+  markdown: {
+    shikiConfig: {
+      themes: {
+        light: "vitesse-light",
+        dark: "vitesse-dark",
+      },
+    },
+  },
 });
